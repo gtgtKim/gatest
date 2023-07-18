2efc7287a0fec91525b780eba6229d34f08d16fd
gtgtKim <kgtrlarbxo@gmail.com>
2023-07-18 10:41:26 +0900

    메인배너 추가

--- productNameChange.js
+++ productNameChange.js
@@ -13,3 +13,27 @@ function changeProductName(productIndex, newProductName) {
     const newProductName = "상품 " + (i + 1);
     changeProductName(i, newProductName);
   }
+
+
+//슬라이드 변경
+  const sliderContainer = document.querySelector('.slider-container');
+  const sliderSlides = document.querySelectorAll('.slider-slide');
+  
+  let slideIndex = 0;
+  
+  function showSlide(index) {
+    sliderSlides.forEach((slide, i) => {
+      slide.style.transform = `translateX(${100 * (i - index)}%)`;
+    });
+  }
+  
+  function nextSlide() {
+    slideIndex++;
+    if (slideIndex >= sliderSlides.length) {
+      slideIndex = 0;
+    }
+    showSlide(slideIndex);
+  }
+  
+  // 매 3초마다 다음 슬라이드 보여주기
+  setInterval(nextSlide, 3000);
