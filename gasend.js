242980e2639bf624c093129783735b93b59f3d2f
gtgtKim <kgtrlarbxo@gmail.com>
2023-07-18 16:09:59 +0900

    데이터레이어 초기화

--- gasend.js
+++ gasend.js
@@ -2,20 +2,22 @@
 // 구매 버튼 클릭 함수 선언
 function gaSend(i) {
  const gaPrice = Number(document.querySelectorAll(".product")[i].querySelector('p').textContent.match(/\d+/));
- const A = i + 1
+ const ii = i + 1
+ const targetObj = {
+    "event":  "select_item",
+    "ecommerce":  {
+     "items": [{
+       "item_id": ii.toString(),
+       "price": gaPrice,
+       "index": ii
+     }],
+     "item_list_name": "상품 리스트",
+     "currency": "USD"
+   }
+ }
+    dataLayer.push(targetObj);
+    dataLayerReset(targetObj);
 
-    dataLayer.push({
-        "event":  "select_item",
-        "ecommerce":  {
-         "items": [{
-           "item_id": A.toString(),
-           "price": gaPrice,
-           "index": A
-         }],
-         "item_list_name": "상품 리스트",
-         "currency": "USD"
-       }
-     });
 }
 
 for (let i = 0; i < buyButtons.length; i++) {
