d157032b6d8059a6f5d79ea42e6010bec93d0999
gtgtKim <kgtrlarbxo@gmail.com>
2023-07-24 17:02:38 +0900

    Update gasend.js

--- gasend.js
+++ gasend.js
@@ -27,7 +27,7 @@ for (let i = 0; i < buyButtons.length; i++) {
 
 
 // "product" 클래스를 가진 모든 div 요소를 선택합니다.
-let elements = document.querySelectorAll('div.product');
+let elements = document.querySelectorAll('div.product-image');
 
 // 첫 번째 요소가 존재하는 경우에만 id를 설정합니다.
 if (elements.length > 0) {
